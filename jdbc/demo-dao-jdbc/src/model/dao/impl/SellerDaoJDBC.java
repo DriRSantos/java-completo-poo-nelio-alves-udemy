08b413de4d640a4d96c1fc3a92130cd56c5a5c27
DriRSantos <drirsantos@outlook.com>
2023-05-10 16:25:45 -0300

    findAll and insert implementation

--- jdbc/demo-dao-jdbc/src/model/dao/impl/SellerDaoJDBC.java
+++ jdbc/demo-dao-jdbc/src/model/dao/impl/SellerDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -27,8 +28,41 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
-
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"INSERT INTO seller "
+					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES (?, ?, ?, ?, ?)", 
+					Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthdate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if(rs.next()) {
+					int id = rs.getInt(1);
+					obj.setId(id);
+				}
+				// rs should be close inside if scope
+				DB.closeResultSet(rs);
+			}	
+			else {
+				throw new DbException("Unexpected error! No rows affected! ");
+			}			
+		}
+		catch (SQLException e){
+			throw new DbException(e.getMessage());
+		}		
+		finally{
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
@@ -97,8 +131,45 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "ORDER BY Name");
+						
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<>();
+			// instantiate map to control Department repeat
+			Map<Integer, Department> map = new HashMap<>();
+			
+			// rs could be one or more values, do while (exist scroll through rs)
+			while (rs.next()) {
+				
+				// instantiate dep to try to find Department with specific id
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				// if dep null instantiate Department
+				if (dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				Seller obj = instantiateSeller(rs, dep);
+				list.add(obj);
+			}
+			return list;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 	
 	@Override
