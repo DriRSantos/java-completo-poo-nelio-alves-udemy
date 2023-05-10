5903e24e2c23a39ed790cb4f19668cd3f356f7a0
DriRSantos <drirsantos@outlook.com>
2023-05-10 16:48:49 -0300

    update and delete implementation

--- jdbc/demo-dao-jdbc/src/application/Program.java
+++ jdbc/demo-dao-jdbc/src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,30 +12,55 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
-		// instantiate SellerDao using DaoFactory, no need to make the implementation explicit
+
+		Scanner sc = new Scanner(System.in);
+
+		// instantiate SellerDao using DaoFactory, no need to make the implementation
+		// explicit
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+
 		System.out.println("=== TEST 1: seller findById =====");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-		
+
 		System.out.println("\n=== TEST 2: seller findByDepartment =====");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+
 		System.out.println("\n=== TEST 3: seller findAll =====");
 		list = sellerDao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
-		System.out.println("\n=== TEST 3: seller insert =====");
+
+		System.out.println("\n=== TEST 4: seller insert =====");
 		Seller newSeller = new Seller(null, "Dri", "dri@email.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+
+		System.out.println("\n=== TEST 5: seller update =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Adriana Neves");
+		seller.setEmail("adriana@email.com");
+		sellerDao.update(seller);
+		System.out.println("Update completede! New name = " + seller.getName() + " | New email = " + seller.getEmail());
+
+		System.out.println("\n=== TEST 6: seller delete =====");
+		System.out.print("Enter id to delete: ");
+		int id = sc.nextInt();
+
+		seller = sellerDao.findById(id);
+
+		if (seller == null) {
+			System.out.println("Seller doesn't exist");
+		} else {
+			sellerDao.deleteById(id);
+			System.out.println("Delete completed. Seller deleted: " + seller.getName());
+		}
+
+		sc.close();
 	}
 }
