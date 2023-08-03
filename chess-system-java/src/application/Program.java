01f63248b57ef3b1d2a7faf41f92167444447d65
DriRSantos <drirsantos@outlook.com>
2023-08-03 15:59:39 -0300

    start implement board, piece, chess layer, printing board

--- chess-system-java/src/application/Program.java
+++ chess-system-java/src/application/Program.java
@@ -2,18 +2,16 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
-
 	public static void main(String[] args) {
-
+		
 		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner (System.in);
+		Scanner sc = new Scanner (System.in);		
 
-		System.out.println("Hello");
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 
 		sc.close();
 	}
